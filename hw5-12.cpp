b58a1fae23231968ee3ad325d601f374b1f0eb78
AndiLarocque <casandrelarocque@my.tridenttech.edu>
2024-07-22 10:25:46 -0400

    Update hw5-12.cpp

--- hw5-12.cpp
+++ hw5-12.cpp
@@ -7,6 +7,26 @@ using namespace std;
 
 int main()
 {
+	float item_cost1; 
+	float item_cost2; 
+	
+	float half=0;
+	float amount_owed=0;
+	cout<< "Enter cost for item 1:"; 
+	cin>>item_cost1;
+	cout<< "Enter cost for item 2:";
+	cin>>item_cost2;
+if (item_cost1 > item_cost2)
 
-	return 0;
-}	//end of main function
+{
+half=item_cost2/2; 
+		amount_owed=item_cost1 + half;
+	}
+else 
+	{	
+		half=item_cost1/2;
+		amount_owed=item_cost2 + half; 
+	}
+
+	cout<<fixed<<setprecision(2);
+	cout<< "Amount owed" << amount_owed;
